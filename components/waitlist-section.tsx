58b701c8c343a6dbc032c1a603caee7aad3287b9
m-mcaulay <hello@moiramcaulay.co.uk>
2025-07-23 16:56:12 +0100

    Remove unused import

--- components/waitlist-section.tsx
+++ components/waitlist-section.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const targetDate = new Date("2025-01-01T00:00:00");
